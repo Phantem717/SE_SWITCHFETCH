adf8b7fa3d66da343b4cf2f0a1c79c8ab6523bee
Souji <YoruKogaSouji@gmail.com>
2024-05-12 16:41:04 +0700

    testing for merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Navbar from "@/scenes/navbar";
-// import SignUp from "./components/signUp";
+import SignUp from "./components/signUp";
 
 function App() {
 
   return (
     <div className='app bg-pageColor'>
       <Navbar />
-      {/* <SignUp /> */}
+      <SignUp />
     </div>
   )
 }
