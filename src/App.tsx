af1a36d27e01df11f1cbfc6f7988a3ed8b10ab61
Phantem717 <pradahsuherli@gmail.com>
2024-05-15 14:47:43 +0700

    Navigateable

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import Navbar from "@/scenes/navbar";
-// import SignUp from "./components/signUp";
+import SignUp from "./components/signUp";
 
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 function App() {
 
   return (
+    <Router>
+
+    
     <div className='app bg-pageColor'>
       <Navbar />
       {/* <SignUp /> */}
+      <Routes>
+      <Route path = "/" element={<Navbar />}></Route>
+        <Route path = "/signup" element={<SignUp />}></Route>
+        
+      </Routes>
     </div>
+    </Router>
   )
 }
 
