6ea6d51bbe92add4eedfffbc4e8142ad4339127e
Souji <YoruKogaSouji@gmail.com>
2024-06-08 18:18:14 +0700

    tweak for login and register

--- src/Pages/register.tsx
+++ src/Pages/register.tsx
@@ -66,14 +66,14 @@ const signUp = () => {
     <div className={'bg-cover h-screen bg-gradient-to-r from-logregGrad5 to-logregGrad6'} style= {{backgroundImage: `url(${background})`}}>
       
         <form action=""  className={`${flexCent}  text-black h-svh`}>
-        <div className={`${flexCent} shadow-xl bg-gradient-to-r from-logregGrad1/50 to-logregGrad2/50 rounded-tl-lg rounded-bl-lg h-4/6 w-4/12`}>
+        <div className={`${flexCent} shadow-xl bg-gradient-to-r from-logregGrad1/50 from-25% via-logregGrad1.5/50 via-50% to-logregGrad2/50 to-77% rounded-tl-lg rounded-bl-lg h-4/6 w-4/12`}>
           <img src={logo} alt="" className={`h-3/4`}/>
           </div>
-            <div className={`shadow-xl bg-gradient-to-r from-logregGrad3/50 to-logregGrad4/50 rounded-tr-lg rounded-br-lg h-4/6 w-3/12 p-12 bg-o `}>
-              <h1 className={`${flexCent} text-3xl`}>Register</h1>
+            <div className={`shadow-xl bg-gradient-to-b from-logregGrad3/50 from-0% via-logregGrad4/50 via-41% to-loregGrad4.5/50 to-100% rounded-tr-lg rounded-br-lg h-4/6 w-3/12 p-12 bg-o `}>
+              <h1 className={`${flexCent} text-3xl font-bold`}>Register</h1>
               
-              <div className={`mt-4`}>
-                <p className={`text-black text-xs font-bold`}>Username</p>
+              <div className={`mt-12`}>
+                <p className={`text-black`}>Username</p>
                 <input className={`hover:placeholder:font-normal hover:placeholder:text-black transition-all duration-300 hover:border-2 hover:border-blue-500  hover:bg-blue-200   pl-2 rounded-lg w-full h-8 text-xs bg-transparent border-solid border-2 border-white text-logregTxt placeholder:text-gray-200`}
                        value={name}
                        onChange={inputUsername}
@@ -81,7 +81,7 @@ const signUp = () => {
               </div>
               
               <div className={`mt-4`}>
-                <p className={`text-black text-xs font-bold`}>Email Address</p>
+                <p className={`text-black`}>Email Address</p>
                 <input className={`hover:placeholder:font-normal hover:placeholder:text-black transition-all duration-300 hover:border-2 hover:border-blue-500  hover:bg-blue-200   pl-2 rounded-lg w-full h-8 text-xs bg-transparent border-solid border-2 border-white text-logregTxt font-normal placeholder:text-gray-200`} type="text"
                        value={email}
                        onChange={inputEmail}
@@ -94,7 +94,7 @@ const signUp = () => {
               {/*</div>*/}
 
               <div className={`mt-4`}>
-                <p className={`text-black text-xs font-bold`}>Password</p>
+                <p className={`text-black`}>Password</p>
                 <input className={`hover:placeholder:font-normal hover:placeholder:text-black transition-all duration-300 hover:border-2 hover:border-blue-500  hover:bg-blue-200   pl-2 rounded-lg w-full h-8 text-xs bg-transparent border-solid border-2 border-white text-logregTxt placeholder:text-gray-200`}
                        value={password}
                        onChange={inputPassword}
@@ -103,8 +103,8 @@ const signUp = () => {
 
               <button className={`${flexCent} mt-5 text-xs h-8 w-full rounded-lg text-white bg-logregBtn cursor-pointer hover:font-bold hover:shadow-md hover:shadow-blue-400 hover:p-1 hover:bg-blue-200  hover:text-black transition-all duration-300 font-medium` } type="submit" onClick={handleRegisterClick}>Register</button>
               
-              <div className={`text-xs mt-8`}>
-                <p className={`${flexCent} text-logregyesAcc`}>Have account already?</p>
+              <div className={`text-xs mt-10`}>
+                <p className={`${flexCent} text-logregnoAcc`}>Have account already?</p>
                 <a className={`${flexCent} text-logregyesAcc hover:underline font-bold transition-all duration-300 `} href="./signUp" >Login Here</a>
               </div>
             </div>
--- src/Pages/signUp.tsx
+++ src/Pages/signUp.tsx
@@ -54,11 +54,11 @@ const signUp = () => {
   return (
     <div className={'bg-cover h-screen bg-gradient-to-r from-logregGrad5 to-logregGrad6'} style= {{backgroundImage: `url(${background})`}}>
         <form action="" className={`${flexCent}  text-black h-svh`}>
-        <div className={`${flexCent} shadow-xl bg-gradient-to-r from-logregGrad1/50 to-logregGrad2/50 rounded-tl-lg rounded-bl-lg h-4/6 w-4/12`}>
+        <div className={`${flexCent} shadow-xl bg-gradient-to-r from-logregGrad1/50 from-25% via-logregGrad1.5/50 via-50% to-logregGrad2/50 to-77% bg-transparent rounded-tl-lg rounded-bl-lg h-4/6 w-4/12`}>
           <img src={logo} alt="" className={`h-3/4`}/>
           </div>
-            <div className={`shadow-xl bg-gradient-to-r from-logregGrad3/50 to-logregGrad4/50 rounded-tr-lg rounded-br-lg h-4/6 w-3/12 p-12`}>
-              <h1 className={`${flexCent} text-3xl`}>Login</h1>
+            <div className={`shadow-xl bg-gradient-to-b from-logregGrad3/50 from-0% via-logregGrad4/50 via-41% to-loregGrad4.5/50 to-100% rounded-tr-lg rounded-br-lg h-4/6 w-3/12 p-12`}>
+              <h1 className={`${flexCent} text-3xl font-bold`}>Login</h1>
               <div className={`mt-12`}>
                 <p className={`text-black`}>Email Address</p>
                 <input className={`hover:placeholder:font-normal hover:placeholder:text-black transition-all duration-300 hover:border-2 hover:border-blue-500  hover:bg-blue-200    placeholder:text-gray-200 rounded-lg w-full h-8 text-xs bg-transparent border-solid border-2 border-white text-logregTxt pl-2 `}
@@ -82,7 +82,7 @@ const signUp = () => {
 
 
               <div className={`text-xs mt-28`}>
-                <p className={`${flexCent} text-logregyesAcc`}>No Account yet?</p>
+                <p className={`${flexCent} text-logregnoAcc`}>No Account yet?</p>
                 <a className={`${flexCent} text-logregyesAcc hover:underline font-bold transition-all duration-300`} 
                 href="./register"
                   >
